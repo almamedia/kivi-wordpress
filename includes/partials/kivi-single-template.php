8653ecd365190ce9769e22b58d71dd86bdae8bb7
kristianahonen <kristian.ahonen@almamedia.fi>
2020-08-18 18:21:25 +0300

    Fix syntax

--- includes/partials/kivi-single-template.php
+++ includes/partials/kivi-single-template.php
@@ -39,7 +39,7 @@ get_header(); ?>
 							$attachment->ID,
 							$size = array("1200", "800"),
 							false,
-							array( "class" => "slick-for-image", "alt" => "", "loading" => false ),
+							array( "class" => "slick-for-image", "alt" => "", "loading" => false )
 				  ); ?>
                 </div>
 			  <?php endforeach; ?>
