c874fdb59eb4f4c7951a48ba026e9c757a439c79
Kristian Ahonen <kristian.ahonen@almamedia.fi>
2021-09-27 15:29:13 +0300

    Reformat code

--- public/kivi-functions.php
+++ public/kivi-functions.php
@@ -9,58 +9,61 @@
  * @author     ktalo <antti.keskitalo@almamedia.fi>
  */
 
-  /**
-  * Template tag for displaying the filters form
-  * @return html object
-  */
-  function map_post_meta($meta_field){
-      return Kivi_Public::map_post_meta($meta_field);
-  }
+/**
+ * Template tag for displaying the filters form
+ * @return html object
+ */
+function map_post_meta( $meta_field ) {
+	return Kivi_Public::map_post_meta( $meta_field );
+}
 
-  function set_kivi_option( $name, $value){
-    $old = get_option('kivi-options');
-    if( ! is_array($old)){ $old=[];}
-    $kivi_options = array_merge( $old, array($name => $value ) );
-    update_option( 'kivi-options', $kivi_options );
-  }
+function set_kivi_option( $name, $value ) {
+	$old = get_option( 'kivi-options' );
+	if ( ! is_array( $old ) ) {
+		$old = [];
+	}
+	$kivi_options = array_merge( $old, array( $name => $value ) );
+	update_option( 'kivi-options', $kivi_options );
+}
 
-  function get_kivi_option( $name ){
-    $kivi_options = get_option('kivi-options');
-    if( is_array($kivi_options) && array_key_exists($name,$kivi_options) ){
-      return $kivi_options[$name];
-    }else{
-      return "";
-    }
-  }
+function get_kivi_option( $name ) {
+	$kivi_options = get_option( 'kivi-options' );
+	if ( is_array( $kivi_options ) && array_key_exists( $name, $kivi_options ) ) {
+		return $kivi_options[ $name ];
+	} else {
+		return "";
+	}
+}
 
-  /*
-  * Used by the index template to populate search criteria for the filtering
-  * of the posts based on the (custom) metadata.
-  */
-  function populate_searchcriteria( &$criteria, &$request, $field, $key, $operator, $intval=false ){
-    if ( isset($request[$field]) && $request[$field] != '' ) {
-      $value = sanitize_text_field($request[$field]);
-      $type='CHAR';
-      if( $intval ){
-          $value = intval($request[$field]);
-          $type = 'NUMERIC';
-      }
-      $criteria = array(
-        'key'     => $key,
-        'value'   => $value,
-        'compare' => $operator,
-        'type'    => $type,
-      );
-    }
-  }
+/*
+* Used by the index template to populate search criteria for the filtering
+* of the posts based on the (custom) metadata.
+*/
+function populate_searchcriteria( &$criteria, &$request, $field, $key, $operator, $intval = false ) {
+	if ( isset( $request[ $field ] ) && $request[ $field ] != '' ) {
+		$value = sanitize_text_field( $request[ $field ] );
+		$type  = 'CHAR';
+		if ( $intval ) {
+			$value = intval( $request[ $field ] );
+			$type  = 'NUMERIC';
+		}
+		$criteria = array(
+			'key'     => $key,
+			'value'   => $value,
+			'compare' => $operator,
+			'type'    => $type,
+		);
+	}
+}
 
-  /*
-  * Helper to get a posted value from the POST/GET request.
-  */
-  function get_posted_value( &$request, $value ){
-    $ret="";
-    if ( isset($request["submit"]) && isset($request[$value]) ){
-      $ret=$request[$value];
-    }
-    return $ret;
-  }
+/*
+* Helper to get a posted value from the POST/GET request.
+*/
+function get_posted_value( &$request, $value ) {
+	$ret = "";
+	if ( isset( $request["submit"] ) && isset( $request[ $value ] ) ) {
+		$ret = $request[ $value ];
+	}
+
+	return $ret;
+}
