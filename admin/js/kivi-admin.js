9786709d2728e630e3cfdd15f1250cf2c57b6768
Kristian Ahonen <kristian.ahonen@almamedia.fi>
2021-09-28 17:02:47 +0300

    Recode admin display

--- admin/js/kivi-admin.js
+++ admin/js/kivi-admin.js
@@ -1,99 +1,73 @@
-(function( $ ) {
-	'use strict';
+(function ($) {
+    'use strict';
 
-	/**
-	* Admin JS stuff.
-	 */
+    /**
+     * Admin JS stuff.
+     */
+    $(function () {
 
-	$(function() {
+        $('#save-kivi-settings').on('click', function () {
+            if ($('#kivi-show-statusbar').val() == '') {
+                $('#kivi-show-statusbar').val(0);
+            }
+            var opts = {
+                'action': 'kivi_save_settings',
+                'kivi-brand-color': $('#kivi-brand-color').val(),
+                'kivi-slug': $('#kivi-slug').val(),
+                'kivi-show-statusbar': $('#kivi-show-statusbar').val(),
+                'kivi-prefilter-name': $('#kivi-prefilter-name').val(),
+                'kivi-prefilter-value': $('#kivi-prefilter-value').val(),
+                'kivi-gmap-id': $('#kivi-gmap-id').val(),
+                'kivi-rest-user': $('#kivi-rest-user').val(),
+                'kivi-rest-pass': $('#kivi-rest-pass').val()
+            };
 
-    $('#save-kivi-settings').on('click', function() {
-      if ( $('#kivi-show-statusbar').val() == '' ) {
-        $('#kivi-show-statusbar').val(0);
-      }
-      var opts = {
-        'action': 'kivi_save_settings',
-        'kivi-brand-color': $('#kivi-brand-color').val(),
-        'kivi-slug': $('#kivi-slug').val(),
-        'kivi-show-statusbar': $('#kivi-show-statusbar').val(),
-        'kivi-prefilter-name': $('#kivi-prefilter-name').val(),
-        'kivi-prefilter-value': $('#kivi-prefilter-value').val(),
-        'kivi-show-sidebar': $('#kivi-show-sidebar').is(':checked') ? true : "",
-		'kivi-use-www-size': $('#kivi-use-www-size').is(':checked') ? true : "",
-        'kivi-use-debt-free-price-on-shortcode': $('#kivi-use-debt-free-price-on-shortcode').is(':checked') ? true : "",
-		'kivi-clean-values': $('#kivi-clean-values').is(':checked') ? true : "",
-        'kivi-gmap-id': $('#kivi-gmap-id').val(),
-		'kivi-remote-url': $('#kivi-remote-url').val(),
-        'kivi-rest-user': $('#kivi-rest-user').val(),
-        'kivi-rest-pass': $('#kivi-rest-pass').val()
-      };
-
-      doAjax(opts, function(res, type) {
-        showMsg(res, type);
-      });
-    });
-
-	$('#xmlimport-sync').on('click', function() {
-      $('body').css('opacity', '0.8');
-      $('body').css('cursor', 'wait');
-      $('#xmlimport-sync').css('cursor', 'wait');
-      $('#kivi-show-statusbar').val(1);
-
-      var opts = {
-        'action': 'kivi_set_remote_url',
-        'kivi-remote-url': $('#kivi-remote-url').val(),
-      };
-      doAjax(opts, function() {
-        doAjax({'action': 'kivi_sync', 'kivi-show-statusbar': $('#kivi-show-statusbar').val()}, function(res, type) {
-          showMsg(res, type);
-          $('body').css('opacity', '1');
-          $('body').css('cursor', 'initial');
-          $('#xmlimport-sync').css('cursor', 'initial');
+            doAjax(opts, function (res, type) {
+                showMsg(res, type);
+            });
         });
-      });
-	});
 
-    $('#rest-update-all').on('click', function() {
+        $('#rest-update-all').on('click', function () {
+            $('body').css('opacity', '0.8');
+            $('body').css('cursor', 'wait');
+            doAjax({'action': 'kivi_sync'}, function (res, type) {
+                showMsg(res, type);
+                $('body').css('opacity', '1');
+                $('body').css('cursor', 'initial');
+            });
 
-      doAjax({'action': 'kivi_sync' }, function(res, type) {
-        showMsg(res, type);
-        $('body').css('opacity', '1');
-      });
-
-    });
-
-    $('#xmlimport-reset').on('click', function() {
-      if (confirm('Oletko varma että haluat keskeyttää ja poistaa jo tuodut kohteet?')) {
-        doAjax( {'action': 'kivi_reset'}, function(res, type) {
-          showMsg(res, type);
         });
-      } else {
-        return false;
-      }
+
+        $('#import-reset').on('click', function () {
+            if (confirm('Oletko varma että haluat keskeyttää ja poistaa jo tuodut kohteet?')) {
+                doAjax({'action': 'kivi_reset'}, function (res, type) {
+                    showMsg(res, type);
+                });
+            } else {
+                return false;
+            }
+        });
+
+        // Add Color Picker
+        $('#kivi-brand-color').wpColorPicker();
     });
 
-    // Add Color Picker
-    $('#kivi-brand-color').wpColorPicker();
+    function doAjax(data, callback) {
+        var jqxhr = $.ajax({type: 'post', url: ajaxurl, data: data, dataType: 'json'})
+            .done(function (res) {
+                callback(res, 'success');
+            })
+            .fail(function (err) {
+                callback(err, 'error');
+            })
+            .always(function (res) {
+                console.log(JSON.stringify(res));
+            });
+    }
 
-	});
+    function showMsg(res, type) {
+        $('#admin-info').addClass('admin-info--' + type).text(res.message);
+        location.reload();
+    }
 
-
-  function doAjax(data, callback) {
-    var jqxhr = $.ajax( { type: 'post', url: ajaxurl, data: data, dataType: 'json' } )
-    .done(function(res) {
-      callback(res, 'success');
-    })
-    .fail(function(err) {
-      callback(err, 'error');
-    })
-    .always(function(res) {
-      console.log(JSON.stringify(res));
-    });
-  }
-
-  function showMsg(res, type) {
-    $('#admin-info').addClass('admin-info--'+type).text(res.message);
-    location.reload();
-  }
-
-})( jQuery );
+})(jQuery);
--- admin/partials/kivi-admin-display.php
+++ admin/partials/kivi-admin-display.php
@@ -27,26 +27,28 @@
             <form method="post" action="options.php" id="xmlform" autocomplete="off">
 				<?php settings_fields( 'kivi-settings' ); ?>
 				<?php do_settings_sections( 'kivi-settings' ); ?>
-                <h2><?php _e( "Aseta URL ja tuo aineisto", "kivi" ); ?>:</h2>
-                <input type="text" name="kivi-remote-url" id="kivi-remote-url" class="text-input"
-                       value="<?php echo esc_attr( get_option( 'kivi-remote-url' ) ); ?>"
-                       placeholder="<?php _e( 'Tähän laitetaan XML-tiedoston URL-osoite', 'kivi' ); ?>">
-                <button type="button" id="xmlimport-sync" class="button button-secondary">
-					<?php _e( "Tuo", "kivi" ); ?>
-                </button>
-                <div class="other-kivi-settings">
-                    <label>
-                        <div><?php _e( "Rajaa tuotavia kohteita", "kivi" ); ?></div>
-                        <div class="description"><?php _e( "Rajaa aineistosta tuotavia kohteita aineiston kenttien mukaan. Esim. nettinäkyvyydelle valitun välittäjän mukaan voi rajata käyttäen kenttää iv_person_name." ); ?></div>
-                        <input type="text" name="kivi-prefilter-name" id="kivi-prefilter-name" class="text-input"
-                               value="<?php echo esc_attr( get_kivi_option( 'kivi-prefilter-name' ) ); ?>"
-                               placeholder="<?php _e( 'elementin nimi',
-							       'kivi' ); ?>"><?php _e( "Esisuodatuksen peruste", "kivi" ); ?>
-                        <input type="text" name="kivi-prefilter-value" id="kivi-prefilter-value" class="text-input"
-                               value="<?php echo esc_attr( get_kivi_option( 'kivi-prefilter-value' ) ); ?>"
-                               placeholder="<?php _e( 'elementin arvo', 'kivi' ); ?>"><?php _e( "Esisuodatuksen arvo",
-							"kivi" ); ?>
-                    </label>
+
+                <div class="kivi-settings kivis-settings-rest">
+
+                    <h3>REST-API</h3>
+		            <?php do_action( 'kivi-admin-rest-form' ); ?>
+
+                    <p><a href="#"
+                          onclick='document.getElementById("kivi-credentials").style.visibility="visible";'>Aseta
+                            tunnukset</a></p>
+                    <div id="kivi-credentials" style="visibility: hidden;">
+                        <label for="kivi-rest-user"><?php _e( "User name", "kivi" ); ?></label>
+                        <input type="password" name="kivi-rest-user" id="kivi-rest-user" class="text-input"
+                               value="" autocomplete="new-password">
+                        <label for="kivi-rest-pass"><?php _e( "Password", "kivi" ); ?></label>
+                        <input type="password" name="kivi-rest-pass" id="kivi-rest-pass" class="text-input"
+                               value="" autocomplete="new-password">
+                    </div>
+
+                </div>
+
+
+                <div class="kivi-settings kivi-settings-other">
 
                     <p>
                         <label for="kivi-brand-color"><?php _e( "Brändiväri", "kivi" ); ?></label>
@@ -55,55 +57,44 @@
                                placeholder="<?php _e( 'Brändiväri, joka esiintyy esim. napeissa jne. Kivi -kohteita näytettäessä',
 							       'kivi' ); ?>">
                     </p>
-                    <label>
-                        <input type="text" name="kivi-slug" id="kivi-slug" class="text-input"
-                               value="<?php echo esc_attr( get_option( 'kivi-slug' ) ); ?>"
-                               placeholder="<?php _e( 'esim. kohde', 'kivi' ); ?>"><?php _e( "Polkutunnus", "kivi" ); ?>
-                    </label>
-                    <label>
-                        <input type="checkbox"
-                               id="kivi-use-debt-free-price-on-shortcode" <?php if ( get_kivi_option( 'kivi-use-debt-free-price-on-shortcode' ) ) {
-							echo "checked=''";
-						} ?> name="kivi-use-debt-free-price-on-shortcode"
-                               value=""><?php _e( 'Näytä velaton hinta shortcode-listauksissa (oletus: myyntihinta)' ) ?>
-                    </label>
-                    <label>
-                        <input type="checkbox"
-                               id="kivi-clean-values" <?php if ( get_kivi_option( 'kivi-clean-values' ) ) {
-							echo "checked=''";
-						} ?> name="kivi-clean-values"
-                               value=""><?php _e( 'Siisti kohdesivun arvot. (Esim. hintoihin €, neliöt m², piilota menneet esittelyt)' ) ?>
-                    </label>
-                    <label><input type="text" name="kivi-gmap-id" id="kivi-gmap-id" class="text-input"
-                                  value="<?php echo get_kivi_option( 'kivi-gmap-id' ) ?>" placeholder="google maps key">Google
-                        maps api key</label>
-                    <input type="hidden" name="kivi-show-statusbar" id="kivi-show-statusbar"
-                           value="<?php echo esc_attr( get_option( 'kivi-show-statusbar' ) ); ?>">
-
-                    <button type="button button-secondary" id="xmlimport-reset"
-                            class="button button-secondary"><?php _e( 'Keskeytä', 'kivi' ); ?></button>
-
-                    <div>
-                        <h3>REST-API</h3>
-						<?php do_action( 'kivi-admin-rest-form' ); ?>
-
-                        <p><a href="#"
-                              onclick='document.getElementById("kivi-credentials").style.visibility="visible";'>Aseta
-                                tunnukset</a></p>
-                        <div id="kivi-credentials" style="visibility: hidden;">
-                            <label for="kivi-rest-user"><?php _e( "User name", "kivi" ); ?></label>
-                            <input type="password" name="kivi-rest-user" id="kivi-rest-user" class="text-input"
-                                   value="" autocomplete="new-password">
-                            <label for="kivi-rest-pass"><?php _e( "Password", "kivi" ); ?></label>
-                            <input type="password" name="kivi-rest-pass" id="kivi-rest-pass" class="text-input"
-                                   value="" autocomplete="new-password">
-                        </div>
-                    </div>
+                    <label for="kivi-slug"><?php _e( "Polkutunnus", "kivi" ); ?></label>
+                    <input type="text" name="kivi-slug" id="kivi-slug" class="text-input"
+                           value="<?php echo esc_attr( get_option( 'kivi-slug' ) ); ?>"
+                           placeholder="<?php _e( 'esim. kohde', 'kivi' ); ?>"
+                    />
 
 
-                    <button type="button" id="save-kivi-settings"
-                            class="button button-secondary"><?php _e( 'Tallenna asetukset', 'kivi' ); ?></button>
+                    <label for="kivi-gmap-id">Google maps api key</label>
+                    <input type="text" name="kivi-gmap-id" id="kivi-gmap-id" class="text-input"
+                           value="<?php echo get_kivi_option( 'kivi-gmap-id' ) ?>"
+                           placeholder="google maps key"
+                    />
+
                 </div>
+
+
+                <div class="kivi-settings">
+
+                    <h3><?php _e( "Rajaa tuotavia kohteita", "kivi" ); ?></h3>
+                    <p class="description"><?php _e( "Rajaa aineistosta tuotavia kohteita aineiston kenttien mukaan. Esim. nettinäkyvyydelle valitun välittäjän sähköpostin mukaan voi rajata käyttäen kenttää _ui_CONTACT_EMAIL." ); ?></p>
+                    <label for="kivi-prefilter-name"><?php _e( "Esisuodatuksen peruste", "kivi" ); ?></label>
+                    <input type="text" name="kivi-prefilter-name" id="kivi-prefilter-name" class="text-input"
+                           value="<?php echo esc_attr( get_kivi_option( 'kivi-prefilter-name' ) ); ?>"
+                           placeholder="<?php _e( 'elementin nimi',
+			                   'kivi' ); ?>">
+                    <label for="kivi-prefilter-value"><?php _e( "Esisuodatuksen arvo",
+				            "kivi" ); ?></label>
+                    <input type="text" name="kivi-prefilter-value" id="kivi-prefilter-value" class="text-input"
+                           value="<?php echo esc_attr( get_kivi_option( 'kivi-prefilter-value' ) ); ?>"
+                           placeholder="<?php _e( 'elementin arvo', 'kivi' ); ?>">
+                </div>
+
+                <button type="button" id="save-kivi-settings"
+                        class="button button-secondary"><?php _e( 'Tallenna asetukset', 'kivi' ); ?></button>
+
+                <button type="button button-secondary" id="import-reset"
+                        class="button button-secondary"><?php _e( 'Keskeytä', 'kivi' ); ?></button>
+
             </form>
 
 			<?php if ( ! wp_next_scheduled( 'kivi_items_sync' ) ) : ?>
