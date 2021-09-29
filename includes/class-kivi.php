cf57d164649532f1defb989bca4cb4936e0ab667
Kristian Ahonen <kristian.ahonen@almamedia.fi>
2021-09-29 17:22:04 +0300

    Add connection test to admin view

--- includes/class-kivi.php
+++ includes/class-kivi.php
@@ -169,8 +169,10 @@ class Kivi {
     $this->loader->add_action( 'wp_ajax_kivi_reset', $plugin_admin, 'kivi_reset' );
     $this->loader->add_action( 'wp_ajax_kivi_set_remote_url', $plugin_admin, 'kivi_set_remote_url' );
     $this->loader->add_action( 'wp_ajax_kivi_save_settings', $plugin_admin, 'kivi_save_settings' );
+	add_action( 'kivi-admin-rest-form', array( 'KiviRest', 'testApiConnection' ) );
 
-    add_filter( 'cron_schedules', 'kivi_add_schedule' );
+
+	  add_filter( 'cron_schedules', 'kivi_add_schedule' );
 
   }
 
