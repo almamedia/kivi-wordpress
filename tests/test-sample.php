09e930dd77b6703dec5b5fcd58c14b2043629d2b
Kristian Ahonen <kristian.ahonen@almamedia.fi>
2021-10-15 15:58:22 +0300

    Replace sample tests

--- tests/test-sample.php
+++ tests/test-sample.php
@@ -1,15 +1,10 @@
 <?php
 
-class SampleTest extends WP_UnitTestCase {
+class KiviFunctionsTest extends WP_UnitTestCase {
 
-	function test_sample() {
-		// replace this with some actual testing code
-		$this->assertTrue( true );
-	}
-
-	function test_sample2() {
-		// replace this with some actual testing code
-		$this->assertTrue( true );
+	function test_kivi_option_setget(){
+		set_kivi_option( 'test', 'testvalue' );
+		$this->assertSame( 'testvalue', get_kivi_option('test') );
 	}
 
 }
