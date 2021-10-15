e78ae006c5845e729e10585f9ab3508f429fc67d
Kristian Ahonen <kristian.ahonen@almamedia.fi>
2021-10-15 13:50:29 +0300

    Test current wordpress-php version compatibility

--- tests/test-sample.php
+++ tests/test-sample.php
@@ -6,5 +6,11 @@ class SampleTest extends WP_UnitTestCase {
 		// replace this with some actual testing code
 		$this->assertTrue( true );
 	}
+
+	function test_sample2() {
+		// replace this with some actual testing code
+		$this->assertTrue( true );
+	}
+
 }
 
